6dd2774128101d68d482e11fcf712732fca919de
serhiihaniuk <serhii.haniuk@gmail.com>
2023-11-07 00:00:39 +0100

    fix click events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
       }}
       className="bg-pattern w-max-[100vw] relative flex min-h-screen flex-col items-center justify-center pb-6 text-white "
     >
-      <div className="fixed inset-0">
+      <div className="pointer-events-none fixed inset-0">
         <div
           className="bg-stars w-max-[100wv] animate-r-360 pointer-events-none left-0 top-0 z-20 h-[750px] w-full bg-cover bg-center bg-no-repeat"
           style={{
@@ -318,46 +318,14 @@ export default function HomePage() {
             </div>
           </div>
         </section>
-        <section className="container grid grid-rows-1 items-center justify-center justify-items-center gap-5 px-5 md:grid-cols-2 md:pt-12 ">
-          <div className="flex max-w-[320px] flex-col items-center justify-center gap-2 self-start md:max-w-[500px]">
-            <h2 className="text-center text-2xl font-bold md:text-2xl">
-              Будь на связи!
-            </h2>
-            <p className="text-center">
-              Переходи в телеграм канал.
-              <a href="https://t.me/+NXNyzuGV84AxYzdk" target="_blank">
-                <svg
-                  className="mx-3 inline-block h-7 w-7"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  version="1.1"
-                  xmlns="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  xmlSpace="preserve"
-                  style={{
-                    fillRule: "evenodd",
-                    clipRule: "evenodd",
-                    strokeLinejoin: "round",
-                    strokeMiterlimit: "1.41421",
-                    color: "#0088cc",
-                  }}
-                >
-                  <path
-                    id="telegram-1"
-                    d="M18.384,22.779c0.322,0.228 0.737,0.285 1.107,0.145c0.37,-0.141 0.642,-0.457 0.724,-0.84c0.869,-4.084 2.977,-14.421 3.768,-18.136c0.06,-0.28 -0.04,-0.571 -0.26,-0.758c-0.22,-0.187 -0.525,-0.241 -0.797,-0.14c-4.193,1.552 -17.106,6.397 -22.384,8.35c-0.335,0.124 -0.553,0.446 -0.542,0.799c0.012,0.354 0.25,0.661 0.593,0.764c2.367,0.708 5.474,1.693 5.474,1.693c0,0 1.452,4.385 2.209,6.615c0.095,0.28 0.314,0.5 0.603,0.576c0.288,0.075 0.596,-0.004 0.811,-0.207c1.216,-1.148 3.096,-2.923 3.096,-2.923c0,0 3.572,2.619 5.598,4.062Zm-11.01,-8.677l1.679,5.538l0.373,-3.507c0,0 6.487,-5.851 10.185,-9.186c0.108,-0.098 0.123,-0.262 0.033,-0.377c-0.089,-0.115 -0.253,-0.142 -0.376,-0.064c-4.286,2.737 -11.894,7.596 -11.894,7.596Z"
-                  />
-                </svg>{" "}
-              </a>
-              Там будет вся актуальная информация по поводу конференции.
-            </p>
-          </div>
+        <section className="container grid grid-rows-1 items-center justify-center justify-items-center gap-5 px-5 md:pt-12 ">
           <div className="mt-5 flex max-w-[320px] flex-col items-center justify-center gap-2 self-start md:mt-0 md:max-w-[500px]">
             <h2 className="text-center text-2xl font-bold md:text-2xl">
               Есть вопросы?{" "}
             </h2>
             <p className="text-center">
-              Напиши нам в поддержку!
               <a target="_blank" href="https://t.me/+THVRcBRhyWc5OWY0">
+                Напиши нам в поддержку!
                 <svg
                   className="ml-5 inline-block h-7 w-7"
                   fill="currentColor"
