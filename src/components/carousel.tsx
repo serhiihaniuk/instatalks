00cb9585a31eefc1e6b74f04016d2b45dd4db616
serhiihaniuk <serhiihaniuk@gmail.com>
2024-03-16 14:53:49 +0100

    fix scroll

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -20,20 +20,15 @@ const HorizontalScrollCarousel = () => {
   const x = useTransform(scrollYProgress, [0, 1], ["1%", "-95%"]);
 
   return (
-    <section ref={targetRef} className="relative h-[200vh]">
-      <h2 className="mb-5 text-start text-3xl font-bold md:text-4xl">
+    <section ref={targetRef} className="relative h-[600vh]">
+      <h2 className="mb-5 text-center text-3xl font-bold md:text-4xl">
         <span
           className={cn(
             "absolute top-0 block h-7 w-7 scale-125 rounded-2xl bg-amber-100 blur-xl",
           )}
         />
-        <span
-          data-content={"Попередня конференція"}
-          className="relative block before:absolute before:inset-y-0 before:left-0 before:z-0 before:block before:w-full before:animate-gradient-background-1 before:bg-gradient-to-r before:from-pink-400 before:to-orange-300 before:bg-clip-text before:px-2 before:text-start before:text-transparent before:content-[attr(data-content)]"
-        >
-          <span className="animate-gradient-foreground-1 bg-gradient-to-r from-gradient-blue to-gradient-teal bg-clip-text px-2 text-start text-transparent">
-            Попередня конференція
-          </span>
+        <span className="animate-gradient-foreground-1 bg-gradient-to-r from-gradient-blue to-gradient-teal bg-clip-text px-2 text-center text-transparent">
+          Попередня конференція
         </span>
       </h2>
       <div className="sticky top-0 flex h-screen items-center overflow-hidden">
