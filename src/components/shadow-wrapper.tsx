5657dc35ccbc04b33f241ee840f8750d98597354
serhiihaniuk <serhiihaniuk@gmail.com>
2024-03-27 11:26:46 +0100

    targen blank

--- src/components/shadow-wrapper.tsx
+++ src/components/shadow-wrapper.tsx
@@ -5,6 +5,7 @@ export const BuyButton = () => {
         <div className="absolute -inset-0.5 animate-tilt rounded-lg bg-gradient-to-r from-[#f59b0e] to-[#f43f5e] text-orange-300 opacity-75 blur transition duration-1000 group-hover:opacity-100 group-hover:duration-200"></div>
         <a
           href="https://buy.stripe.com/cN2bKI81x8YNg9idR0"
+          target="_blank"
           className="relative flex items-center rounded-lg bg-[#022016] px-7 py-4 text-lg font-normal leading-none text-orange-300"
         >
           Забронювати
